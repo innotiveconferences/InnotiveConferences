cfb5a1df266ae39701688c3acd7d09a774e215da
Shaikzaheer174 <shaikzaheer174@gmail.com>
2026-07-11 10:44:53 +0530

    fixed issues'

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,5 @@
 import { defineConfig } from "vite";
 
-const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1] || "";
-const base = process.env.BASE_PATH
-  ? `/${process.env.BASE_PATH}/`
-  : repoName
-    ? `/${repoName}/`
-    : "/";
-
 export default defineConfig({
-  base,
+  base: "/",
 });
